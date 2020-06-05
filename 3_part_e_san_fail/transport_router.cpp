f51c6636000890d19bf0221e182fb64a27289800
bogdan <lakatosh@phystech.edu>
2020-06-05 21:50:42 +0300

    Reached sanitizer problems

--- 3_part_e_san_fail/transport_router.cpp
+++ 3_part_e_san_fail/transport_router.cpp
@@ -32,8 +32,8 @@ void TransportRouter::FillGraphWithStops(const Descriptions::StopsDict& stops_di
     auto& vertex_ids = stops_vertex_ids_[stop_name];
     vertex_ids.in = vertex_id++;
     vertex_ids.out = vertex_id++;
-    vertices_info_[vertex_ids.in] = {stop_name};
-    vertices_info_[vertex_ids.out] = {stop_name};
+    vertices_info_.push_back({stop_name});
+    vertices_info_.push_back({stop_name});
 
     edges_info_.push_back(WaitEdgeInfo{});
     graph_.AddEdge({
@@ -55,13 +55,13 @@ void TransportRouter::FillGraphWithBuses(const Descriptions::StopsDict& stops_di
       continue;
     }
     auto compute_distance_from = [&stops_dict, &bus](size_t lhs_idx) {
-      return Descriptions::ComputeStopsDistance(*stops_dict.at(bus.stops[lhs_idx]), *stops_dict.at(bus.stops[lhs_idx + 1]));
+      return Descriptions::ComputeStopsDistance(*stops_dict.at(bus.stops[lhs_idx-1]), *stops_dict.at(bus.stops[lhs_idx]));
     };
-    for (size_t start_stop_idx = 0; start_stop_idx + 1 < stop_count; ++start_stop_idx) {
+    for (size_t start_stop_idx = 0; start_stop_idx < stop_count - 1; ++start_stop_idx) {
       const Graph::VertexId start_vertex = stops_vertex_ids_[bus.stops[start_stop_idx]].in;
       int total_distance = 0;
-      for (size_t finish_stop_idx = start_stop_idx + 1; finish_stop_idx <= stop_count; ++finish_stop_idx) {
-        total_distance += compute_distance_from(finish_stop_idx - 1);
+      for (size_t finish_stop_idx = start_stop_idx + 1; finish_stop_idx < stop_count; ++finish_stop_idx) {
+        total_distance += compute_distance_from(finish_stop_idx);
         edges_info_.push_back(BusEdgeInfo{
             .bus_name = bus.name,
             .span_count = finish_stop_idx - start_stop_idx,
